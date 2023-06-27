fa5316400c70430d49a236db9780f101dd2dc19e
Florian Matter <florianmatter@gmail.com>
2023-06-27 14:05:42 -0700

    label & anchor

--- src/clld_markdown_plugin/__init__.py
+++ src/clld_markdown_plugin/__init__.py
@@ -82,18 +82,17 @@ def link_entity(req, objid, route, model, session, decorate=None, ids=None, **kw
     if objid == "__all__":
         if ids:
             md_strs = [
-                link_entity(req, mid, route, model, session, decorate=decorate)
+                link_entity(req, mid, route, model, session, decorate=decorate, **kwargs)
                 for mid in ids[0].split(",")
             ]
             return comma_and_list(md_strs)
         raise NotImplementedError("Table not yet implemented")  # pragma: no cover
     else:
         entity = session.query(model).filter(model.id == objid)[0]
-        anchor = kwargs.pop("_anchor", None)
-        if isinstance(anchor, list):
-            anchor = anchor[0]
+        label = kwargs.pop("label", [None])[0]
+        anchor = kwargs.pop("_anchor", [None])[0]
         url = req.route_url(route, id=objid, _anchor=anchor, **kwargs)
-        md_str = f"""<a class="{model.__tablename__.capitalize()}" href="{url}">{entity.name}</a>"""
+        md_str = f"""<a class="{model.__tablename__.capitalize()}" href="{url}">{label or entity.name}</a>"""
         return decorate(md_str) if decorate else md_str
 
 
